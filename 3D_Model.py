981412fd2d185ce73383905edbf323672d462440
Luca(PC) <halflife390@gmail.com>
2020-01-08 22:20:05 +0000

    Added 3D model file to repo

---
+++ 3D_Model.py
@@ -0,0 +1,35 @@
+"""
+IDP 3 Group 9 - Tidal Lagoon Mathematical Model
+Module: 3D Model of Lagoon
+Gianluca Cantone 2019
+"""
+
+#Libraries ====================================================================
+import math as m
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+#Variables ==================================================================== 
+  
+
+
+#Main Program =================================================================
+
+Count = int(input("Enter number of tierations: "))
+
+for i in range(Count):    
+    print(str(i+1) + ": " + str((i+1)**2))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
