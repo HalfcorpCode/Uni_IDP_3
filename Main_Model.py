6eb1ba40255f3230c8ec1215fe94eb152d46a5c0
Luca(PC) <halflife390@gmail.com>
2020-02-07 01:26:15 +0000

    Added economic analysis, power and energy generated, simple head loss across turbine and fluid velocity & discharge over time

--- Main_Model.py
+++ Main_Model.py
@@ -24,15 +24,21 @@ import datetime
 Global_Time = [0]
 Global_Volume = [0]  
 Global_Tide = [0]
-Global_Head = [0] 
+Global_Head = [0]
+Global_Head_Difference = [0]
+Global_Velocity = [0]
+Global_Discharge = [0] 
 Profile_List = []
+Global_Head_Loss = [0]
+Global_Power = [0]
+Global_Power_Elec = []
 
-Civil_Price = 0
-Blade_Price = 0
-Turbine_Price = 0
-Gearbox_Price = 0
-Generator_Price = 0
-Sluice_Price = 0
+Civil_Price = 10000000
+Blade_Price = 20000
+Turbine_Price = 50000
+Gearbox_Price = 150000
+Generator_Price = 100000000
+Sluice_Price = 30000
 
 #Main Program =================================================================
 print("\n")
@@ -55,21 +61,30 @@ def Run_Simulation(**kwargs):
     Econ = kwargs["econ"]
     Output = kwargs["output"]
     Graphs = kwargs["graphs"]
+    Graph_Head = kwargs["graph_head"]
+    Graph_QV = kwargs["graph_QV"]
+    Graph_P = kwargs["graph_P"]
     
     #Efficiency terms:
     
-    Eff_Turbine = 0
-    Eff_Gearbox = 0
-    Eff_Generator = 0
+    Eff_Turbine = 0.9
+    Eff_Gearbox = 0.85
+    Eff_Generator = 0.97
     
     #Global Variables
     
     global Civil_Price, Blade_Price, Turbine_Price, Gearbox_Price, Generator_Price, Sluice_Price
-    global Global_Time, Global_Volume, Global_Tide, Global_Head
+    global Global_Time, Global_Volume, Global_Tide, Global_Head, Global_Head_Difference, Global_Velocity, Global_Discharge, Global_Head_Loss, Global_Power, Global_Power_Elec
     Global_Time = [0]
     Global_Volume = [0]  
     Global_Tide = [0]
     Global_Head = [0]
+    Global_Head_Difference = [0]
+    Global_Velocity = [0]
+    Global_Discharge = [0]
+    Global_Head_Loss = [0]
+    Global_Power = [0]
+    Global_Power_Elec = []
     
     #Local Variables
     
@@ -81,6 +96,7 @@ def Run_Simulation(**kwargs):
     Time = [0]
     M = 1453217
     G = 9.807
+    rho = 997
     Tidal_Function = 0
     Discharge_Coefficient= 0.65
     Sluicing_Discharge_Coefficient = 0.95
@@ -89,6 +105,8 @@ def Run_Simulation(**kwargs):
     Operational_Profile = Profile_List[Profile_Number-1]
     Profile_Stage = 0
     Total_Stages = len(Operational_Profile)
+    Total_Mechanical_Energy = 0
+    Total_Electrical_Energy = 0
     #Initial Calculations
     
     Area = np.pi*np.power((Turbine_Diameter/2),2)*Turbines
@@ -131,6 +149,11 @@ def Run_Simulation(**kwargs):
                     Global_Tide.append(6*np.cos(2*np.pi*0.0000231*Current_Time-np.pi)+6)
                     Global_Volume.append(Global_Volume[-1])
                     Global_Head.append((Global_Volume[-1])/(M))
+                    Global_Head_Difference.append(Global_Head[-1]-Global_Tide[-1])
+                    Global_Velocity.append(0)
+                    Global_Discharge.append(0)
+                    Global_Head_Loss.append(0)
+                    Global_Power.append(0)
                     Current_Time += 1
             
         if State == 1:
@@ -156,10 +179,17 @@ def Run_Simulation(**kwargs):
                         Global_Volume.append(Global_Volume[-1])
                         Profile_Stage = (Profile_Stage+1)%Total_Stages
                         State = Operational_Profile[Profile_Stage][0]
+                        Global_Velocity.append(Global_Velocity[-1])
+                        Global_Discharge.append(Global_Discharge[-1])
                     else:    
                         Global_Volume.append(Global_Volume[Current_Time-1]+Step_Size*Sluice_Area*Sluicing_Discharge_Coefficient*np.sqrt(2*G)*np.sqrt((Global_Tide[Current_Time])-((Global_Volume[Current_Time-1])/(M))-Pipe_Loss))
-                    
+                        Global_Velocity.append(np.sqrt(2*G)*np.sqrt((Global_Tide[Current_Time])-((Global_Volume[Current_Time-1])/(M))-Pipe_Loss))
+                        Global_Discharge.append(Global_Velocity[-1]*Sluice_Area*Sluicing_Discharge_Coefficient)
+                        
                     Global_Head.append((Global_Volume[Current_Time])/(M))
+                    Global_Head_Difference.append(Global_Head[-1]-Global_Tide[-1])
+                    Global_Head_Loss.append(0)
+                    Global_Power.append(0)
                     Current_Time += 1
                     #print("the time is: " + str(Current_Time))
         
@@ -186,10 +216,19 @@ def Run_Simulation(**kwargs):
                         Global_Volume.append(Global_Volume[-1])
                         Profile_Stage = (Profile_Stage+1)%Total_Stages
                         State = Operational_Profile[Profile_Stage][0]
+                        Global_Velocity.append(Global_Velocity[-1])
+                        Global_Discharge.append(Global_Discharge[-1])
+                        Global_Head_Loss.append(Global_Head_Loss[-1])
+                        Global_Power.append(Global_Power[-1])
                     else:
                         Global_Volume.append(Global_Volume[Current_Time-1]-Step_Size*Area*Discharge_Coefficient*np.sqrt(2*G)*np.sqrt((Global_Volume[Current_Time-1])/(M)-(Global_Tide[Current_Time])-Turbine_Loss-Pipe_Loss))
+                        Global_Velocity.append(np.sqrt(2*G)*np.sqrt((Global_Volume[Current_Time-1])/(M)-(Global_Tide[Current_Time])-Turbine_Loss-Pipe_Loss))
+                        Global_Discharge.append(Global_Velocity[-1]*Area*Discharge_Coefficient)
+                        Global_Head_Loss.append(0.5*Global_Velocity[-1])
+                        Global_Power.append((rho/G)*Global_Discharge[-1]*0.5*Global_Velocity[-1])
                     
                     Global_Head.append((Global_Volume[Current_Time])/(M))
+                    Global_Head_Difference.append(Global_Head[-1]-Global_Tide[-1])
                     Current_Time += 1
             
         if State == 3:
@@ -208,18 +247,44 @@ def Run_Simulation(**kwargs):
         print("Current time: " + str(Current_Time))    
     
     #Energy generation calculations
+    print("\n================================================================")
+    print("Running energy calculations...")
     
-    #Log headloss across turbine at every step and store in an array
-    #Multiply each value by thing to get energy/power
-    #Make graph of power over time and total cumlative energy generated
+    for i in Global_Power:
+        Global_Power_Elec.append(i*Eff_Turbine*Eff_Gearbox*Eff_Generator)
+        Total_Mechanical_Energy += i
+        Total_Electrical_Energy += Global_Power_Elec[-1]
+
+    Energy_Lost = Total_Mechanical_Energy-Total_Electrical_Energy
+    Efficiency = (Eff_Turbine*Eff_Gearbox*Eff_Generator)
+    
+    print("Total mechanical energy generated: " + str(Total_Mechanical_Energy))
+    print("Energy lost: " + str(Energy_Lost))
+    print("System efficiency: " + str(Efficiency))
+    print("Total electrical energy generated (J): " + str(Total_Electrical_Energy))
+    print("Total electrical energy generated (kWh): " + str(Total_Electrical_Energy/3.6e+6))
     #Print peak power and total energy
-    #Pass energy through inefficiencies of electrical system
     #Print final power and energy and percentage lost
-    
     
     if Econ == True:                                                            #Section for calculating economic assessment.
         
+        print("\n================================================================")
         print("Running economic assessment of configuration...")
+        
+        Startup_Costs = Civil_Price+(Turbines*(Blade_Price+Turbine_Price+Gearbox_Price+Generator_Price+Sluice_Price))+(Sluices*Sluice_Price)
+        Running_Costs_Day = 1000 #a day
+        Total_Running_Costs = Running_Costs_Day*(Run_Time/86400)
+        Energy_Price = 50/1000 #per kWh
+        Turnover = (Total_Electrical_Energy/(3.6e+6))*Energy_Price
+        Gross_Profit = Turnover-Total_Running_Costs
+        Net_Profit = Gross_Profit-Startup_Costs
+        
+        print("Startup costs: " + str(Startup_Costs))
+        print("Running costs: " + str(Total_Running_Costs))
+        print("Turnover: " + str(Turnover))
+        print("Gross profit: " + str(Gross_Profit))
+        print("Net profit " + str(Net_Profit))
+        
     #Fixed costs - Start up costs, running costs, etc
     #Variable costs
     #Take into account maintencance downtimes
@@ -230,16 +295,21 @@ def Run_Simulation(**kwargs):
         
         ax = plt.axes()
         second_ax = ax.twinx()
-        plt.title("Forward Euler")
+        plt.title("Lagoon Volume, Head and Tide Vs Time")
         ax.set_xlabel("Time (s)")
         ax.set_ylabel("Volume (m^3)")
         second_ax.set_ylabel('Head (m)')
        
-        Filling_Plot = ax.plot(Global_Time, Global_Volume, label="Lagoon Volume", color="deepskyblue", linewidth=2)
-        Lagoon_Head_Plot = second_ax.plot(Global_Time, Global_Head, "--", label="Lagoon head", color="green")
-        Tide_Height_Plot = second_ax.plot(Global_Time, Global_Tide, "--", label="Tide height", color ="blue")
+        Filling_Plot = ax.plot(Global_Time, Global_Volume, label="Lagoon Volume", color="deepskyblue", linewidth=3)
+        Lagoon_Head_Plot = second_ax.plot(Global_Time, Global_Head, "--", label="Lagoon head", color="green", linewidth=2)
+        Tide_Height_Plot = second_ax.plot(Global_Time, Global_Tide, "--", label="Tide height", color ="blue", linewidth=2)
         
-        Lines = Filling_Plot+Lagoon_Head_Plot+Tide_Height_Plot
+        if Graph_Head == True:  
+            Head_Difference_Plot = second_ax.plot(Global_Time, Global_Head_Difference, "--", label="Head difference", color ="orange", linewidth=2)
+            Lines = Filling_Plot+Lagoon_Head_Plot+Tide_Height_Plot+Head_Difference_Plot
+        else:
+            Lines = Filling_Plot+Lagoon_Head_Plot+Tide_Height_Plot
+            
         Labels =[l.get_label() for l in Lines]
         ax.legend(Lines, Labels)
         
@@ -247,6 +317,45 @@ def Run_Simulation(**kwargs):
         ax.grid(which='major', color='black', linestyle='-', linewidth=1)
         ax.grid(which='minor', color='black', linestyle='--', linewidth=0.5)
         ax.set_ylim(0,2e7)
+        
+        if Graph_QV == True:
+            
+            #Red graphs
+            
+            plt.figure(figsize=plt.figaspect(1)*2)
+            QVax = plt.axes()
+            second_QVax = QVax.twinx()
+            plt.title("Velocity & Discharge Vs Time")
+            QVax.set_xlabel("Time (s)")
+            QVax.set_ylabel("Velocity (ms^-1)")
+            second_QVax.set_ylabel('Discharge (m^3s^-1)')
+            
+            Velocity_Plot = QVax.plot(Global_Time, Global_Velocity, label="Flow velocity", color="red", linewidth=2)
+            Discharge_Plot = second_QVax.plot(Global_Time, Global_Discharge, "--", label="Total discharge", color="maroon", linewidth=2)
+            Lines = Velocity_Plot+Discharge_Plot
+            Labels =[l.get_label() for l in Lines]
+            QVax.legend(Lines, Labels)
+            plt.minorticks_on()
+            QVax.grid(which='major', color='black', linestyle='-', linewidth=1)
+            QVax.grid(which='minor', color='black', linestyle='--', linewidth=0.5)
+            
+        if Graph_P == True:
+            
+            plt.figure(figsize=plt.figaspect(1)*2)
+            Pax = plt.axes()
+            plt.title("Mechanical & Electrical Power Vs Time")
+            Pax.set_xlabel("Time (s)")
+            Pax.set_ylabel("Power (w)")
+            
+            Mech_Power_Plot = Pax.plot(Global_Time, Global_Power, label="Mechanical Power", color="gold", linewidth=2)
+            Elec_Power_Plot = Pax.plot(Global_Time, Global_Power_Elec, "--", label="Electrical Power", color="y", linewidth=2)
+            Lines = Mech_Power_Plot+Elec_Power_Plot
+            Labels =[l.get_label() for l in Lines]
+            Pax.legend(Lines, Labels)
+            plt.minorticks_on()
+            Pax.grid(which='major', color='black', linestyle='-', linewidth=1)
+            Pax.grid(which='minor', color='black', linestyle='--', linewidth=0.5)
+            
     
 
 def Print_Costs():
